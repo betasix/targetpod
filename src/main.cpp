1adb8bcb3a42189368fe5deeb03993fde1740782
simon <simon@localhost.localdomain>
2019-01-27 10:41:42 +0100

    [Added support for new led] - using the WS2812 library to support new LED type

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 #include <Arduino.h>
 #include <avr/sleep.h>
 #include <EEPROM.h>
-#include "color.hpp"
+#include <WS2812.h>
+#include "Color.hpp"
 
-constexpr uint8_t pin_analog_red = 7;
-constexpr uint8_t pin_analog_green = 6;
-constexpr uint8_t pin_analog_blue = 8;
-constexpr uint8_t pin_switch_dimmer = 9;
-constexpr uint8_t pin_switch_color = 10;
-constexpr float m_dimmStepSize = 0.05f;
-constexpr float m_roundPrecision = 0.5f;
+constexpr uint8_t pin_led_cmd = 3;
+constexpr uint8_t pin_switch_dimmer = 1;
+constexpr uint8_t pin_switch_color = 0;
+constexpr uint8_t m_magicNumberForEEPROM = 42;
+constexpr uint8_t m_memoryPosition_magicNumber = 0;
+constexpr uint8_t m_memoryPosition_selectedColor = 0;
+constexpr uint8_t m_memoryPosition_brightness = 2;
 constexpr uint8_t m_debouncing_ms_switch_color = 50;
 constexpr uint8_t m_debouncing_ms_switch_brightness = 2;
 constexpr float m_minimumBrightness = 0.02f;
 constexpr float m_maximumBrightness = 1.0f;
-constexpr uint8_t m_magicNumberForEEPROM = 42;
-constexpr uint8_t m_memoryPosition_magicNumber = 0;
-constexpr uint8_t m_memoryPosition_selectedColor = 1;
-constexpr uint8_t m_memoryPosition_brightness = 2;
+constexpr float m_dimmStepSize = 0.05f;
+constexpr float m_roundPrecision = 0.5f;
+static float m_brightness = 1.0f;
 
 static Color red(255, 0, 0);
 static Color green(0, 255, 0);
@@ -26,10 +26,11 @@ static Color pink(255, 0, 255);
 static Color yellow(0, 255, 255);
 static Color teal(255, 255, 0);
 static Color white(255, 255, 255);
-
 Color colors[7] = {red, green, blue, pink, yellow, teal, white};
 
-static float m_brightness = 1.0f;
+static cRGB m_led_value;
+static WS2812 LED(1);
+
 static bool m_brightnessUp = false;
 static uint8_t m_magicNumberReadFromEEPROM = 0;
 
@@ -38,9 +39,11 @@ static uint8_t maxColors = sizeof(colors) / sizeof(Color);
 
 void setRGB(uint8_t r, uint8_t g, uint8_t b)
 {
-    analogWrite(pin_analog_red, r);
-    analogWrite(pin_analog_green, g);
-    analogWrite(pin_analog_blue, b);
+    m_led_value.r = r;
+    m_led_value.g = g;
+    m_led_value.b = b;
+    LED.set_crgb_at(0, m_led_value);
+    LED.sync();
 }
 
 void writeConfigToEEProm()
@@ -72,9 +75,12 @@ void changeColor(bool onlyBrightness)
     {
         selectedColor = 0;
     }
-    setRGB(floor(colors[selectedColor].r() * m_brightness + m_roundPrecision),
-           floor(colors[selectedColor].g() * m_brightness + m_roundPrecision),
-           floor(colors[selectedColor].b() * m_brightness + m_roundPrecision));
+    setRGB
+    (
+        floor(colors[selectedColor].r() * m_brightness + m_roundPrecision),
+        floor(colors[selectedColor].g() * m_brightness + m_roundPrecision),
+        floor(colors[selectedColor].b() * m_brightness + m_roundPrecision)
+    );
     writeConfigToEEProm();
 }
 
@@ -124,20 +130,14 @@ void setup()
     ACSR |= _BV(ACD);                // Disable the analog comparator
     MCUCR |= _BV(BODS) | _BV(BODSE); // Turn off the brown-out detector
 
-    // Changing green timer (TIMER1) to be in sync with the other colors
-    // and remove flickering
-    TCCR1A = 0;
-    TCCR1B = 0;
-    TCCR1A |= (1 << WGM10);
-    TCCR1B |= (1 << WGM12);
-    TCCR1B |= (1 << CS10);
-
     // Enable interrupt functions for button pins
     GIFR = (0 << PCIF1);     // Reset interrupt flag
     PCMSK1 |= (1 << PCINT9); // Pin mask for PCINT9 interrupt
     PCMSK1 |= (1 << PCINT8); // Pin mask for PCINT8 interrupt
     GIMSK |= (1 << PCIE1);   // enable PCINT interrupts
     sei();
+    // setup LED pin
+    LED.setOutput(pin_led_cmd);
 
     // Set initial color to red on start
     setRGB(255, 0, 0);
@@ -148,18 +148,14 @@ void setup()
     // Setup digital I/O-pins
     pinMode(pin_switch_color, INPUT);
     pinMode(pin_switch_dimmer, INPUT);
-
-    // setup anlog pins
-    pinMode(pin_analog_red, OUTPUT);
-    pinMode(pin_analog_green, OUTPUT);
-    pinMode(pin_analog_blue, OUTPUT);
+   
 
     // Send controller to sleep
     set_sleep_mode(SLEEP_MODE_PWR_DOWN); // do a complete power-down
     sleep_enable();                      // enable sleep mode
 }
 
-// Interrupt Service routing for PCINT1 interrupts
+// Interrupt Service routine for PCINT1 interrupts
 ISR(PCINT1_vect)
 {
     if (digitalRead(pin_switch_color) == LOW)
