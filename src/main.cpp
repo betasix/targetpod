47cebab112cd3066a0a2b93b37d632013678c0d0
simon <simon@localhost.localdomain>
2019-01-27 11:40:53 +0100

    [Switch off feature] - when pressing both buttons the brightnessis set to 0

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,16 @@
 constexpr uint8_t pin_led_cmd = 3;
 constexpr uint8_t pin_switch_dimmer = 1;
 constexpr uint8_t pin_switch_color = 0;
-constexpr uint8_t m_magicNumberForEEPROM = 42;
-constexpr uint8_t m_memoryPosition_magicNumber = 0;
-constexpr uint8_t m_memoryPosition_selectedColor = 0;
-constexpr uint8_t m_memoryPosition_brightness = 2;
-constexpr uint8_t m_debouncing_ms_switch_color = 50;
-constexpr uint8_t m_debouncing_ms_switch_brightness = 2;
-constexpr float m_minimumBrightness = 0.02f;
-constexpr float m_maximumBrightness = 1.0f;
-constexpr float m_dimmStepSize = 0.05f;
-constexpr float m_roundPrecision = 0.5f;
+constexpr uint8_t k_magicNumberForEEPROM = 42;
+constexpr uint8_t k_memoryPosition_magicNumber = 0;
+constexpr uint8_t k_memoryPosition_selectedColor = 0;
+constexpr uint8_t k_memoryPosition_brightness = 2;
+constexpr uint8_t k_debouncing_ms_switch_color = 50;
+constexpr uint8_t k_debouncing_ms_switch_brightness = 2;
+constexpr float k_minimumBrightness = 0.02f;
+constexpr float k_maximumBrightness = 1.0f;
+constexpr float k_dimmStepSize = 0.05f;
+constexpr float k_roundPrecision = 0.5f;
 static float m_brightness = 1.0f;
 
 static Color red(255, 0, 0);
@@ -26,7 +26,7 @@ static Color pink(255, 0, 255);
 static Color yellow(0, 255, 255);
 static Color teal(255, 255, 0);
 static Color white(255, 255, 255);
-Color colors[7] = {red, green, blue, pink, yellow, teal, white};
+Color colors[7] = {blue, pink, yellow, teal, white, red, green};
 
 static cRGB m_led_value;
 static WS2812 LED(1);
@@ -48,19 +48,19 @@ void setRGB(uint8_t r, uint8_t g, uint8_t b)
 
 void writeConfigToEEProm()
 {
-    EEPROM.write(m_memoryPosition_magicNumber, m_magicNumberForEEPROM);
-    EEPROM.put(m_memoryPosition_selectedColor, selectedColor);
-    EEPROM.put(m_memoryPosition_brightness, m_brightness);
+    EEPROM.write(k_memoryPosition_magicNumber, k_magicNumberForEEPROM);
+    EEPROM.put(k_memoryPosition_selectedColor, selectedColor);
+    EEPROM.put(k_memoryPosition_brightness, m_brightness);
 }
 
 void readConfigFromEEProm()
 {
-    m_magicNumberReadFromEEPROM = EEPROM.read(m_memoryPosition_magicNumber);
+    m_magicNumberReadFromEEPROM = EEPROM.read(k_memoryPosition_magicNumber);
 
-    if (m_magicNumberReadFromEEPROM == 42)
+    if (m_magicNumberReadFromEEPROM == k_magicNumberForEEPROM)
     {
-        EEPROM.get(m_memoryPosition_selectedColor, selectedColor);
-        EEPROM.get(m_memoryPosition_brightness, m_brightness);
+        EEPROM.get(k_memoryPosition_selectedColor, selectedColor);
+        EEPROM.get(k_memoryPosition_brightness, m_brightness);
     }
 }
 
@@ -77,33 +77,33 @@ void changeColor(bool onlyBrightness)
     }
     setRGB
     (
-        floor(colors[selectedColor].r() * m_brightness + m_roundPrecision),
-        floor(colors[selectedColor].g() * m_brightness + m_roundPrecision),
-        floor(colors[selectedColor].b() * m_brightness + m_roundPrecision)
+        floor(colors[selectedColor].r() * m_brightness + k_roundPrecision),
+        floor(colors[selectedColor].g() * m_brightness + k_roundPrecision),
+        floor(colors[selectedColor].b() * m_brightness + k_roundPrecision)
     );
-    writeConfigToEEProm();
+    //writeConfigToEEProm();
 }
 
 void changeBrightness()
 {
-    if (m_brightness < m_minimumBrightness)
+    if (m_brightness < k_minimumBrightness)
     {
         m_brightnessUp = true;
     }
 
-    if (m_brightness == m_maximumBrightness)
+    if (m_brightness == k_maximumBrightness)
     {
         m_brightnessUp = false;
     }
 
     if (m_brightnessUp == true)
     {
-        m_brightness += m_dimmStepSize;
+        m_brightness += k_dimmStepSize;
     }
 
     if (m_brightnessUp == false)
     {
-        m_brightness -= m_dimmStepSize;
+        m_brightness -= k_dimmStepSize;
     }
 
     changeColor(true);
@@ -123,7 +123,7 @@ void interrupt_button_change_brightness(void)
 void setup()
 {
     // Read config from EEPROM
-    readConfigFromEEProm();
+    //readConfigFromEEProm();
 
     // Power saving configs
     ADCSRA &= ~(1 << ADEN);          // Turn off ADC
@@ -136,11 +136,9 @@ void setup()
     PCMSK1 |= (1 << PCINT8); // Pin mask for PCINT8 interrupt
     GIMSK |= (1 << PCIE1);   // enable PCINT interrupts
     sei();
+    
     // setup LED pin
     LED.setOutput(pin_led_cmd);
-
-    // Set initial color to red on start
-    setRGB(255, 0, 0);
 
     // Set to color from EEPROM if it has been set
     changeColor(true);
@@ -160,14 +158,30 @@ ISR(PCINT1_vect)
 {
     if (digitalRead(pin_switch_color) == LOW)
     {
+        if (digitalRead(pin_switch_dimmer) == LOW)
+        {
+            if (m_brightness == 0.0f)
+            {
+                m_brightness = 1.0f;
+            }
+            else 
+            {
+                m_brightness = 0.0f;
+            }
+            return;
+        }
+        
+    }
+    if (digitalRead(pin_switch_color) == LOW)
+    {
         changeColor(false);
-        delay(m_debouncing_ms_switch_color);
     }
     if (digitalRead(pin_switch_dimmer) == LOW)
     {
         changeBrightness();
-        delay(m_debouncing_ms_switch_brightness);
     }
+    GIFR = (0 << PCIF1);     // Reset interrupt flag
+
 }
 
 // Not used at all
